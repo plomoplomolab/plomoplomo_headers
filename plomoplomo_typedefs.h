d16f962542e15af763e69b8d64fd9814ef0cb7ba
silverio <wsilverio@users.noreply.github.com>
2018-08-19 14:02:59 -0300

    ErrorStatus, FlagStatus

---
+++ plomoplomo_typedefs.h
@@ -0,0 +1,12 @@
+/*
+    arquivo: plomoplomo_typedefs.h
+    2018 - plomoplomo
+*/
+
+#ifnedf PLOMOPLOMO_TYPEDEFS_H
+#define PLOMOPLOMO_TYPEDEFS_H
+
+typedef enum {ERROR = 0, SUCCESS = !ERROR} ErrorStatus;
+typedef enum {RESET = 0, SET = !RESET} FlagStatus;
+
+#endif // PLOMOPLOMO_TYPEDEFS_H
